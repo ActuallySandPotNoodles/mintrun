86c69780d10219f1352065bb23d78613a8708c2b
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2025-08-10 20:14:00 +0100

    Fixed it

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 echo "Installing MintRun!"
 echo "Part of 'Now that's what I call MintMix!'"
 echo "Downloading stuff..."
-wget [what]
-wget [huh]
+wget https://github.com/ActuallySandPotNoodles/mintrun/releases/download/Release/run
+#wget for a not yet created desktop file
 pip install --break-system-packages pyside6
 echo "Actually installing it..."
 echo "Root is reqired to install"
 sudo mv ./run /bin/run
-mv run.desktop ~/.local/share/applications
+#mv run.desktop ~/.local/share/applications/run.desktop
 echo "Done!"
 echo "All you have to do now is test it. Enjoy!"
