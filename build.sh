c14aace6797b85737e6d736b625679310abe1112
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2025-08-10 21:18:00 +0100

    fixed onefile issue

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ wget https://raw.githubusercontent.com/ActuallySandPotNoodles/mintrun/refs/heads
 pip install --break-system-packages pyinstaller
 pip install --break-system-packages pyside6
 echo "Building using PyInstaller..."
-pyinstaller --one-file run.py
+pyinstaller --onefile run.py
 echo 'Set your directory to where the run executable is and then run the installmybuild.sh script'
 echo 'Done!'
